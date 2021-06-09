841eae94d0a6ddffa9600f40def85e6ce02cc27b
Nahuel Franco <nawelfc@gmail.com>
2021-06-08 23:37:52 -0300

    agregamos un parrafo

--- resources/views/cursos/category.blade.php
+++ resources/views/cursos/category.blade.php
@@ -4,4 +4,5 @@
 
 @section('content')
     <h1>Bienvenido al curso {{$curso}} de la categoría {{$categoria}}.</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga animi optio, facere earum eligendi nemo expedita repudiandae facilis sint velit illum sunt nulla molestias corrupti dolorem excepturi iusto est harum.</p>
 @endsection
